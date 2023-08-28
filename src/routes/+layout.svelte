ff8f9a86754259329c04f6d107f904bd48c8dea5
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-28 16:52:08 +0200

    Added: Comments and RoutedSideBar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
-   
+    import RoutedSideBar from "../components/RoutedSideBar/RoutedSideBar.svelte";
 </script>
 
+<!-- This is the most left sidebar used to navigate the application between modes -->
+<!-- Application modes: Editor, Extensions, <Extentions navigations> -->
+<RoutedSideBar />
 
 <slot />
