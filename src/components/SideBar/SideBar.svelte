f26859537dd7e7779b736e8eb8e11721c6b17894
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-29 01:02:15 +0200

    Update SideBar.svelte

---
+++ src/components/SideBar/SideBar.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    let width = 180;
+    let resize_width = 20;
+    let offset = 0;
+    let can_drag = false;
+
+    let innerWidth = 0;
+    // let max_width = 
+
+    function handleMousedown(event : MouseEvent) {
+        const mouseX = event.clientX;
+        const mouse_on_panel = mouseX;
+
+        offset = mouse_on_panel - width;
+
+        if (mouse_on_panel < width && mouse_on_panel > width - resize_width) {
+            can_drag = true;
+        } 
+    }
+
+    function handleMouseUp() {
+        can_drag = false;
+    }
+
+    function handleMouseMove(event : MouseEvent) {
+        if (can_drag) {
+            width = event.clientX - offset - 0;
+            console.log(width);
+            console.log(innerWidth)
+            if (width < 180) {width = 180}
+            if (width > innerWidth * 0.65) {width = innerWidth * 0.65}
+        }
+    } 
+
+
+</script>
+
+<svelte:window  on:mouseup={handleMouseUp} on:mousedown={handleMousedown} on:mousemove={handleMouseMove}  bind:innerWidth/>
+
+<div id="panel" class="SideBar" style="width: {width}px;">
+
+</div>
+
+
+<style> 
+    .SideBar {
+        min-width: 180px;
+        margin-top: 30px;
+        height: calc(100vh - 30px);
+        max-width: 65vw;
+        background-color: rgb(59, 59, 59);
+        border-radius: 0px 0px 0px 10px ;
+    }
+</style>
