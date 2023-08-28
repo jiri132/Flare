345f49d945886bd2ac889f9372feb67840d0bd70
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-29 01:02:11 +0200

    Update RoutedSideBar.svelte

---
+++ src/components/RoutedSideBar/RoutedSideBar.svelte
@@ -0,0 +1,18 @@
+<script>
+
+
+</script>
+
+<div class="RoutedSideBar">
+</div>
+
+<style>
+    .RoutedSideBar {
+        min-width: 50px;
+        max-width: 50px;
+        background-color: rgb(34, 34, 34);
+        color: whitesmoke;
+        height: 100vh;
+    }
+
+</style>
