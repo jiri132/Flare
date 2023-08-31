eb7a03ace8aae2270bac96b586a0268ebe551a68
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-31 20:19:03 +0200

    Created: Contents of display

---
+++ src/components/SideBar/SideBarButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+    enum fileType {
+        directory = 0,
+        file = 1
+    }
+
+    export let _fileType : fileType;
+    export let _fileName : string;
+    export let _filePath : string;
+
+    function interaction() {
+        switch(_fileType) {
+            case fileType.directory:
+                console.log(_fileType);  
+                break;
+            case fileType.file:
+                console.log(_fileType); 
+                break;           
+        }
+    }
+
+</script>
+
+<div on:click={interaction}>
+    {_fileName}
+</div>
+
+
+<style>
+
+</style>
