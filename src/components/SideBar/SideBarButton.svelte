4ce86452dc33804db4d8e87a710330a9be7933f4
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-31 20:49:45 +0200

    Possible to open directories

--- src/components/SideBar/SideBarButton.svelte
+++ src/components/SideBar/SideBarButton.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import type { FileEntry } from "@tauri-apps/api/fs";
+    const self = arguments[0].__proto__.constructor;
 
     enum fileType {
         directory = 0,
@@ -6,16 +8,20 @@
     }
 
     export let _fileType : fileType;
+    export let _directoryChildren : FileEntry[] = [];
     export let _fileName : string;
     export let _filePath : string;
+
+    let show_childeren : boolean = false;
 
     function interaction() {
         switch(_fileType) {
             case fileType.directory:
-                console.log(_fileType);  
+                // console.log(_directoryChildren); 
+                show_childeren = !show_childeren; 
                 break;
             case fileType.file:
-                console.log(_fileType); 
+
                 break;           
         }
     }
@@ -25,8 +31,24 @@
 <div on:click={interaction}>
     {_fileName}
 </div>
+<div class="children">
+    {#if show_childeren} 
+    {#each _directoryChildren as child}
+        {#if child.children !== undefined} 
+            <svelte:component this={self}  _fileName={child.name} _filePath={child.path}  _fileType={0} _directoryChildren={child.children} />
+        {:else}
+            <svelte:component this={self}  _fileName={child.name} _filePath={child.path} _fileType={1} />
+        {/if}
+    {/each}
+    {/if}
+</div>
+
 
 
 <style>
-
+    .children {
+        padding-left: 20px;
+        /* border: solid red; */
+        border-left: 2px solid red;
+    }
 </style>
