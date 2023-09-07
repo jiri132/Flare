5202cb8f8893c3c3061fcff18bc44696eee93aee
jiri132 <jiri28beurskens@gmail.com>
2023-09-07 08:16:05 +0200

    Removed: Unnecesarry rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs;
 fn main() {
-  let paths = fs::read_dir("./").unwrap();
-
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
-    }
-
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![show_in_folder])
     .run(tauri::generate_context!())
