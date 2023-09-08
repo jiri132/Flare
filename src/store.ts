24c149d21ef418e02e52ed4bd9798e943442bf73
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-09-08 20:09:18 +0200

    added: purple theme in store

--- src/store.ts
+++ src/store.ts
@@ -6,5 +6,6 @@ export const Content : Writable<string> = writable("");
 
 import type { theme } from "./themes/theme.interface";
 import { dark } from "./themes/dark.theme";
+import { purple } from "./themes/purple.theme"
 
-export const ActiveTheme : Writable<theme> = writable(dark);
+export const ActiveTheme : Writable<theme> = writable(purple);
