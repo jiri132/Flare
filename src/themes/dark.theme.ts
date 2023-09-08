294f2714e71b81996263d126d8113fbc5902da7b
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-09-08 19:46:45 +0200

    Added dark theme

--- src/themes/dark.theme.ts
+++ src/themes/dark.theme.ts
@@ -1,3 +1,18 @@
-export const dark = {
-    
+import type { theme } from "./theme.interface";
+
+export const dark : theme = {
+    primary: "#31dd31",
+    secondary: "#3f3f3f",
+    SideBar: {
+        text_color: "#fafafa",
+        hover_color: "rgba(0,0,0,0.3)",
+        header_color: "#272727",
+        standard_color: "#3f3f3f",
+        viewing_file_color: undefined
+    },
+    RootView: {
+        standard_color: "#1a1a1a",
+        highlighted_line_color: undefined,
+        active_file_indicator: undefined
+    }
 }
