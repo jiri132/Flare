771bace0e4fa28d0e55a5890214539038bf09c39
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-09-08 19:46:02 +0200

    Active theme store

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,10 @@
 import { writable, type Writable } from "svelte/store";
 
+
 export const Content : Writable<string> = writable("");
+
+
+import type { theme } from "./themes/theme.interface";
+import { dark } from "./themes/dark.theme";
+
+export const ActiveTheme : Writable<theme> = writable(dark);
