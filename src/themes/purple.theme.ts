e21b6c42df622737f2ad9e43f9b4f8dddf075d59
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-09-08 20:09:31 +0200

    Create purple.theme.ts

---
+++ src/themes/purple.theme.ts
@@ -0,0 +1,18 @@
+import type { theme } from "./theme.interface";
+
+export const purple : theme = {
+    primary: "#7a3c8d",
+    secondary: "#af00fa",
+    SideBar: {
+        text_color: "#fff000",
+        // hover_color: "rgba(0,0,0,0.3)",
+        header_color: "#aa1dfa",
+        standard_color: "#8f2d8a",
+        viewing_file_color: undefined
+    },
+    RootView: {
+        standard_color: "#1a1a1a",
+        highlighted_line_color: undefined,
+        active_file_indicator: undefined
+    }
+}
